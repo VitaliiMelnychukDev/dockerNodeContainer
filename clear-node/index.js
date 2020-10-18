4ab6ede31f55ec1de69b616685e47e7add786294
DESKTOP-40JDJ1K\Vitalii <vitaliimelnychuk1992@gmail.com>
2020-10-18 18:11:46 +0300

    create index ts, add linter and prettier

--- clear-node/index.js
+++ clear-node/index.js
@@ -1,12 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const http = require('http');
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    const response = {
-        "message": "Hello"
-    };
-    res.write(JSON.stringify(response));
-    res.end();
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  const response = {
+    message: 'Hello',
+  };
+  res.write(JSON.stringify(response));
+  res.end();
 });
 
 server.listen(8080);
